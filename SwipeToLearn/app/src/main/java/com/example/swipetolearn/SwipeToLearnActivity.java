89af6f32c001035bd87bbc409e7e34cfd316a2a1
Thibault-Goedert <thibault.goedert@student.yncrea.com>
2021-02-13 16:38:38 +0100

    Démarrage projet
    Creation des pages de co, score, et de jeux.
    Creation des fichiers de code principaux

---
+++ SwipeToLearn/app/src/main/java/com/example/swipetolearn/SwipeToLearnActivity.java
@@ -0,0 +1,8 @@
+package com.example.swipetolearn;
+
+import android.app.Activity;
+
+public class SwipeToLearnActivity extends Activity {
+
+
+}
---
+++ SwipeToLearn/app/src/main/java/com/example/swipetolearn/SwipeToLearnLoginActivity.java
@@ -0,0 +1,35 @@
+package com.example.swipetolearn;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.provider.SyncStateContract;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class SwipeToLearnLoginActivity extends Activity implements View.OnClickListener {
+
+    private EditText mLoginEditText;
+    private EditText mPasswordEditText;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+
+        mLoginEditText = (EditText) findViewById(R.id.loginEditText);
+        mPasswordEditText = (EditText) findViewById(R.id.passwordEditText);
+
+        findViewById(R.id.loginButton).setOnClickListener(this);
+    }
+
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
