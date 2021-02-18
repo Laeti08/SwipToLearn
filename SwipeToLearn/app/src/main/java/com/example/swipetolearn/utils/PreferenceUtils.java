0d133d4b49799d4ec377a9551a93087af0846800
Thibault-Goedert <thibault.goedert@student.yncrea.com>
2021-02-18 11:24:16 +0100

    MAJ login-layout et ajout de classes utils

---
+++ SwipeToLearn/app/src/main/java/com/example/swipetolearn/utils/PreferenceUtils.java
@@ -0,0 +1,39 @@
+package com.example.swipetolearn.utils;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+
+import com.example.swipetolearn.SwipeToLearnApplication;
+
+
+public class PreferenceUtils {
+
+	@SuppressWarnings("unused")
+	private static SharedPreferences getSharedPreferences(Context context){
+		return context.getSharedPreferences(Constants.Preferences.SHARED_PREFERENCES_FILE_NAME, Context.MODE_PRIVATE);
+	}
+	
+	private static SharedPreferences getSharedPreferences(){
+		return SwipeToLearnApplication.getContext().getSharedPreferences(Constants.Preferences.SHARED_PREFERENCES_FILE_NAME, Context.MODE_PRIVATE);
+	}
+	
+	public static String getLogin(){
+		final SharedPreferences prefs = getSharedPreferences();
+		return prefs.getString(Constants.Preferences.PREF_LOGIN, null);
+	}
+	
+	public static void setLogin(String login){
+		final SharedPreferences prefs = getSharedPreferences();
+		prefs.edit().putString(Constants.Preferences.PREF_LOGIN, login).commit();
+	}
+	
+	public static String getPassword(){
+		final SharedPreferences prefs = getSharedPreferences();
+		return prefs.getString(Constants.Preferences.PREF_PASSWORD, null);
+	}
+	
+	public static void setPassword(String password){
+		final SharedPreferences prefs = getSharedPreferences();
+		prefs.edit().putString(Constants.Preferences.PREF_PASSWORD, password).commit();
+	}
+}
